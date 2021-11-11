a1e8d6081a8a2e0940d39da903e25d06bdebfc26
octaviolafourcade <octavio.lafourcade@gmail.com>
2021-11-10 23:11:44 -0500

    [ ADD ] Implementation of Edition mode [ ADD ] Error message in case is empty

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,14 @@ function App() {
   const [tarea, setTarea] = useState('')
   const [tareas, setTareas] =useState([])
   const [modoEdicion, setModoEdicion] = useState(false)
+  const [id, setId] = useState('')
+  const [error, setError] = useState(null)
 
   const agregarTarea = e => {
     e.preventDefault()
     if(!tarea.trim()){
       console.log('Elemento vacío')
+      setError('Elemento vacío')
       return
     }
 
@@ -19,6 +22,7 @@ function App() {
       nombreTarea: tarea}
     ])
     setTarea('')
+    setError(null)
   }
 
   const eliminarTarea = id => {
@@ -32,8 +36,27 @@ function App() {
   const editar = item => {
     setModoEdicion(true)
     setTarea(item.nombreTarea)
+    setId(item.id)
     console.log(item)
   }
+
+  const editarTarea = e => {
+    e.preventDefault()
+    if(!tarea.trim()){
+      console.log('Elemento vacío')
+      setError('Elemento vacío')
+      return
+    }
+
+    const arrayEditado = tareas.map(
+      item => item.id === id ? {id, nombreTarea: tarea} : item)
+    setTareas(arrayEditado)
+    setModoEdicion(false)
+    setTarea('')
+    setId('')
+    setError(null)
+  }
+
   return (
     <div className="container">
       <h1 className="text-center" >CRUD Simple</h1>
@@ -41,7 +64,11 @@ function App() {
       <div className="row">
         <div className="col-8">
           <h4 className="text-center">Lista de tareas</h4>
-          <ul className="list-group">
+          {tareas.length === 0 ? (
+            <li className="list-group-item">No hay tareas</li>
+          ) :
+          (
+            <ul className="list-group">
             {
               tareas.map(item => (
                 <li className="list-group-item" key={item.id}>
@@ -52,12 +79,17 @@ function App() {
               ))
             }
           </ul>
+          )}
         </div>
         <div className="col-4">
           <h4 className='text-center'>{
             modoEdicion? 'Editar Tarea': 'Agregar Tarea'
-          }</h4>
-          <form onSubmit={agregarTarea}>
+          }
+          </h4>
+          <form onSubmit={modoEdicion ? editarTarea : agregarTarea}>
+            {
+              error ? <span className='text-danger'> {error}</span> : null
+            }
             <input 
             type="text" 
             className="form-control"
